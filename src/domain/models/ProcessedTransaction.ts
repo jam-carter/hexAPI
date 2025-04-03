5604050b2b50d0247cbd2aa4d02783e4362c7e6a
Jamaul Alexander Aaron <127161564+jam-carter@users.noreply.github.com>
2025-04-03 19:45:19 +0800

    - unused schema definition removal

--- src/domain/models/ProcessedTransaction.ts
+++ src/domain/models/ProcessedTransaction.ts
@@ -17,22 +17,3 @@ export interface IProcessedTransaction {
     type: "purchase" | "stock";
     response: TransactionResponse;
 }
-
-const responseSchema = new Schema(
-    {
-        transactionId: { type: String, required: true },
-        version: { type: Number, required: true },
-        coins: Number,
-        amount: Number
-    },
-    { _id: false }
-);
-
-const processedTransactionSchema = new Schema({
-    transactionId: { type: String, required: true, unique: true },
-    type: { type: String, enum: ["purchase", "stock"], required: true },
-    response: { type: responseSchema, required: true }
-});
-
-export const ProcessedTransaction =
-    models.ProcessedTransaction ?? model("ProcessedTransaction", processedTransactionSchema);
